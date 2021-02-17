919c06aec73c94502d2234dfcd8b97f91ebc0f37
Fernando Ballesteros santamaria <fballest@c1r2s3.42madrid.com>
2021-02-17 14:31:45 +0100

    ft_strlen  a medias

--- ft_strlen.s
+++ ft_strlen.s
@@ -1,13 +1,27 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: fballest <fballest@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/02/10 10:58:16 by fballest          #+#    #+#              #
-#    Updated: 2021/02/10 10:58:32 by fballest         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+; # **************************************************************************** #
+; #                                                                              #
+; #                                                         :::      ::::::::    #
+; #    ft_strlen.s                                        :+:      :+:    :+:    #
+; #                                                     +:+ +:+         +:+      #
+; #    By: fballest <fballest@student.42.fr>          +#+  +:+       +#+         #
+; #                                                 +#+#+#+#+#+   +#+            #
+; #    Created: 2021/02/10 10:58:16 by fballest          #+#    #+#              #
+; #    Updated: 2021/02/17 14:16:07 by fballest         ###   ########.fr        #
+; #                                                                              #
+; # **************************************************************************** #
+
+global  _ft_strlen
+    section     .text
+ft_strlen:
+            xor         rax, rax                ; contador a cero
+            jmp         count                   ; comparador
+plus:
+            inc         rax                     ; incrementador
+count:
+            cmp         BYTE [rdi + rax], 0     ; condicion comparador
+            jne         plus                    ; Ejecuta si se cumple
+done:
+            ret
+
 
 
