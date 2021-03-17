146c124e2b0dd8ef12fe6a1885f652b7b02fe49c
Fernando Ballesteros santamaria <fballest@c1r3s5.42madrid.com>
2021-03-17 13:14:57 +0100

    Aupa

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fballest <fballest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 12:42:07 by fballest          #+#    #+#             */
-/*   Updated: 2021/03/15 13:50:57 by fballest         ###   ########.fr       */
+/*   Updated: 2021/03/16 10:05:15 by fballest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,25 @@ int		main()
 	int		i;
 	long	r;
 	char	buffer[100];
+	char	*s1;
+	char	*s2;
+	char	*s3;
+	char	*s4;
+	char	*s5;
 	char	*lstr;
+	char	*s6;
+	char	*s7;
+	char	*s8;
 
+	s1 = "";
+	s2 = "F";
+	s3 = "Fer";
+	s4 = "Fernando Ballesteros !!!";
+	s5 = "Hola mundo cruel, donde me estará esperando, ya es la hora de comer";
 	lstr = "El juego, en su forma más tradicional, consiste en tres postes verticales. En uno de los postes se apila un número indeterminado de discos perforados por su centro (elaborados de madera), que determinará la complejidad de la solución. Por regla general se consideran siete discos. Los discos se apilan sobre uno de los postes en tamaño decreciente de abajo arriba. No hay dos discos iguales, y todos ellos están apilados de mayor a menor radio -desde la base del poste hacia arriba- en uno de los postes, quedando los otros dos postes vacíos. El juego consiste en pasar todos los discos desde el poste ocupado (es decir, el que posee la torre) a uno de los otros postes vacíos. Para realizar este objetivo, es necesario seguir tres simples reglas";
+	s6 = "1";
+	s7 = "42";
+	s8 = "1984928479182374261367012876";
 	// char	*tmp;
 	// char	*tmp2;
 
@@ -49,23 +65,28 @@ int		main()
 	printf(ANSI_COLOR_BLUE	"\n----------------------------\n" ANSI_COLOR_RESET);
 	printf(ANSI_COLOR_BLUE	"TEST DEL FT_STRLEN VS STRLEN\n" ANSI_COLOR_RESET);
 	printf(ANSI_COLOR_BLUE	"----------------------------\n" ANSI_COLOR_RESET);
-	STRLEN("")
-	if (ft_strlen("") != (int)strlen(""))
+	STRLEN(s1)
+	if (ft_strlen(s1) != (int)strlen(s1))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	STRLEN("hola")
-	if (ft_strlen("hola") != (int)strlen("hola"))
+	STRLEN(s2)
+	if (ft_strlen(s2) != (int)strlen(s2))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	STRLEN("hola mundo")
-	if (ft_strlen("hola mundo") != (int)strlen("hola mundo"))
+	STRLEN(s3)
+	if (ft_strlen(s3) != (int)strlen(s3))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	STRLEN("hola mundo cruel, donde me estará esperando, ya es la hora de comer");
-	if (ft_strlen("hola mundo cruel, donde me estará esperando, ya es la hora de comer") != (int)strlen("hola mundo cruel, donde me estará esperando, ya es la hora de comer"))
+	STRLEN(s4)
+	if (ft_strlen(s4) != (int)strlen(s4))
+		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
+	else
+		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
+	STRLEN(s5);
+	if (ft_strlen(s5) != (int)strlen(s5))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
@@ -74,18 +95,27 @@ int		main()
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	STRLEN("42")
-	if (ft_strlen("42") != (int)strlen("42"))
+	STRLEN(s6)
+	if (ft_strlen(s6) != (int)strlen(s6))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	STRLEN("1");
-	if (ft_strlen("1") != (int)strlen("1"))
+	STRLEN(s7);
+	if (ft_strlen(s7) != (int)strlen(s7))
 		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
 	else
 		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
-	printf(ANSI_COLOR_YELLOW "\n----------\nFINALIZADO\n----------\n" ANSI_COLOR_RESET);
+	STRLEN(s8);
+	if (ft_strlen(s8) != (int)strlen(s8))
+		printf(ANSI_COLOR_RED	"TEST FALLIDO\n" ANSI_COLOR_RESET);
+	else
+		printf(ANSI_COLOR_GREEN	"TEST CORRECTO\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_YELLOW "\n----------------------\nFINALIZADO TEST STRLEN\n----------------------\n\n" ANSI_COLOR_RESET);
 
+
+	printf(ANSI_COLOR_BLUE	"\n----------------------------\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"TEST DEL FT_STRCMP VS STRCMP\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"----------------------------\n" ANSI_COLOR_RESET);
 	// printf("\n--strcmp\n");
 	// STRCMP("", "")
 	// STRCMP("toto", "toto")
@@ -96,21 +126,33 @@ int		main()
 	// printf("`%s`:`%s` = %d\n", NULL, "TOTO", ft_strcmp(NULL, "TOTO"));
 	// printf("`%s`:`%s` = %d\n", NULL, NULL, ft_strcmp(NULL, NULL));
 	// printf("-done\n");
+	printf(ANSI_COLOR_YELLOW "\n----------------------\nFINALIZADO TEST STRCMP\n----------------------\n\n" ANSI_COLOR_RESET);
 
+	printf(ANSI_COLOR_BLUE	"\n----------------------------\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"TEST DEL FT_STRCPY VS STRCPY\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"----------------------------\n" ANSI_COLOR_RESET);
 	// printf("\n--strcpy\n");
 	// printf("`%s` (`toto`)\n", ft_strcpy(buffer, "toto"));
 	// printf("`%s` (empty)\n", ft_strcpy(buffer, ""));
 	// printf("`%s` (`long message`)\n", ft_strcpy(buffer, "long message"));
 	// printf("`%s` (NULL > not modified)\n", ft_strcpy(buffer, NULL));
 	// printf("-done\n");
+	printf(ANSI_COLOR_YELLOW "\n----------------------\nFINALIZADO TEST STRCPY\n----------------------\n\n" ANSI_COLOR_RESET);
 
+	printf(ANSI_COLOR_BLUE	"\n--------------------------\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"TEST DEL FT_WRITE VS WRITE\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"--------------------------\n" ANSI_COLOR_RESET);
 	// printf("\n--write\n");
 	// WRITE("toto", 4L)
 	// WRITE("totototo", 4L)
 	// WRITE("totototo", 8L)
 	// WRITE("toto", 2L)
 	// printf("-done\n");
+	printf(ANSI_COLOR_YELLOW "\n---------------------\nFINALIZADO TEST WRITE\n---------------------\n\n" ANSI_COLOR_RESET);
 
+	printf(ANSI_COLOR_BLUE	"\n------------------------\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"TEST DEL FT_READ VS READ\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"------------------------\n" ANSI_COLOR_RESET);
 	// printf("\n--read (Makefile)\n");
 	// READ(buffer, 50)
 	// READ(buffer, 25)
@@ -119,7 +161,11 @@ int		main()
 	// READ(buffer, 14)
 	// READ(buffer, 0)
 	// printf("-done\n");
+	printf(ANSI_COLOR_YELLOW "\n--------------------\nFINALIZADO TEST READ\n--------------------\n\n" ANSI_COLOR_RESET);
 
+	printf(ANSI_COLOR_BLUE	"\n----------------------------\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"TEST DEL FT_STRDUP VS STRDUP\n" ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_BLUE	"----------------------------\n" ANSI_COLOR_RESET);
 	// printf("\n--ft_strdup\n");
 	// tmp2 = ft_strdup("toto");
 	// DUP(tmp2)
@@ -129,6 +175,7 @@ int		main()
 	// DUP("")
 	// DUP(NULL)
 	// printf("-done\n");
+	printf(ANSI_COLOR_YELLOW "\n----------------------\nFINALIZADO TEST STRDUP\n----------------------\n\n" ANSI_COLOR_RESET);
 
 	// return (0);
 }
