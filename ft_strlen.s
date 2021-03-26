c73f2b24305bb5621e5976ad06d7235032b2a25b
Fernando Ballesteros <fballest@student.42madrid.com>
2021-03-26 15:45:44 +0100

    Unificando

--- ft_strlen.s
+++ ft_strlen.s
@@ -13,20 +13,11 @@
 global  _ft_strlen
 _ft_strlen:
             mov         rax, 0                  ; contador a cero
-<<<<<<< HEAD
             jmp         count                   ; comparador
-=======
-            jmp         count                   ; salta a comparador
->>>>>>> 21157d83160d2d6f2ec7456603037cd2acdd72e2
 plus:
             inc         rax                     ; incrementador de rax que será el retorno
 count:
-<<<<<<< HEAD
             cmp         BYTE [rdi + rax], 0     ; condicion comparador
             jne         plus                    ; Ejecuta si no se cumple la condición anterior
-=======
-            cmp         BYTE [rdi + rax], 0     ; condicion comparador para comprobar si se ha terminado
-            jne         plus                    ; Ejecuta si no se cumple
->>>>>>> 21157d83160d2d6f2ec7456603037cd2acdd72e2
 done:
             ret
